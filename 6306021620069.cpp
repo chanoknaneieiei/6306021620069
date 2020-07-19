a891b1e89905448f3ba0a4bfc155686d5ec4f153
chanoknaneieiei <68503180+chanoknaneieiei@users.noreply.github.com>
2020-07-19 20:07:56 +0700

    Add files via upload

---
+++ 6306021620069.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	cout<<"1st Term of 2020 \t \\45132114 Wanthanee Prachuasupakij"<<endl;
+	cout<<"\n--------------------------------------------------------------------------"<<endl;
+	cout<<"| Subject ID | Subject name \t \t| Score (100) | Grad |"<<endl;
+	cout<<"--------------------------------------------------------------------------"<<endl;
+	cout<<"| 4570034 | Introdoction to computer \t\|\t85 \t| \"A\"| "<<endl;
+	cout<<"| 4580247 | English \t\t\t|\t74\t| \"B\"| "<<endl;
+	cout<<"| 4530001 | Design Thinking \t\t|\t92\t| \"A\"| "<<endl;
+	cout<<"--------------------------------------------------------------------------"<<endl;
+	cout<<"|\t Total \t| Subject =3 \t\t| Average =3.87 \t\b\b\b|"<<endl;
+	cout<<"====================================*==="<<endl;
+	cout<<"\n\nKMUTNB"<<endl;
+}
+
+	
