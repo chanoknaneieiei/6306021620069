d02c2c64689817407bc3ac77b63f96d0ae544e1a
chanoknaneieiei <68503180+chanoknaneieiei@users.noreply.github.com>
2020-07-28 18:08:59 +0700

    Add files via upload

--- 6306021620069.cpp
+++ 6306021620069.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
 void main()
 {
-	cout<<"1st Term of 2020 \t \\45132114 Wanthanee Prachuasupakij"<<endl;
-	cout<<"\n--------------------------------------------------------------------------"<<endl;
-	cout<<"| Subject ID | Subject name \t \t| Score (100) | Grad |"<<endl;
-	cout<<"--------------------------------------------------------------------------"<<endl;
-	cout<<"| 4570034 | Introdoction to computer \t\|\t85 \t| \"A\"| "<<endl;
-	cout<<"| 4580247 | English \t\t\t|\t74\t| \"B\"| "<<endl;
-	cout<<"| 4530001 | Design Thinking \t\t|\t92\t| \"A\"| "<<endl;
-	cout<<"--------------------------------------------------------------------------"<<endl;
-	cout<<"|\t Total \t| Subject =3 \t\t| Average =3.87 \t\b\b\b|"<<endl;
-	cout<<"====================================*==="<<endl;
-	cout<<"\n\nKMUTNB"<<endl;
+	string first,last;
+	int saly,sale,commis,total;
+	cout<<"Enter Name :";
+	cin>> first >>last;
+	cout<<"Enter Saraly :";
+	cin>> saly;
+	cout<<"Enter Sale :";
+	cin>> sale;
+	cout<<"Enter Commisstion Percent :";
+	cin>> commis;
+	cout<<"\n---------------output------------";
+	cout<<"\nYour name :"<<first<<"\t"<<last;
+	total = (sale*commis/100+saly);
+	cout<<"\nTotal Revenue :\t"<<total<<"\tBath\n";
 }
-
-	
